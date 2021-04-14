2114d2823056ec6970615a857711d16d5915f2c7
LincolnCFCruz <79378712+LincolnCFCruz@users.noreply.github.com>
2021-04-14 20:08:15 -0300

    criar lista

---
+++ souce/lista.h
@@ -0,0 +1,37 @@
+#ifndef lista
+#define lista 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Produto{
+  int codigo;
+  char descricao[100];
+  float valor;
+  int quantidade;
+}Produto;
+
+typedef struct Celula{
+  Produto dado;
+  struct Celula *prox;
+}Celula;
+
+Celula *new_celula(){
+  Celula *tmp = (Celula*)malloc(sizeof(Celula));
+  tmp->prox = NULL;
+  return tmp;
+}
+
+typedef struct Lista{
+  Celula *inicio;
+  Celula *fim;
+  int tam;
+}Lista;
+
+void new_lista(Lista *l);{
+  l->inicio - l -> fim = new_celula();
+  l->tam = 0;
+}
+
+#endif
